fa3bf1875bab29b7baf803aa4b3e10e3b46a78d6
Elvin González <60045534+elvinlab@users.noreply.github.com>
2021-06-20 21:30:42 -0600

    finished screen profile, color change in btn login, library optimization

--- app/src/main/java/com/example/barbershop/MainActivity.java
+++ app/src/main/java/com/example/barbershop/MainActivity.java
@@ -1,32 +1,12 @@
 package com.example.barbershop;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.viewpager2.widget.ViewPager2;
 
-import com.bumptech.glide.Glide;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.tabs.TabLayout;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,36 +14,10 @@ public class MainActivity extends AppCompatActivity {
     ViewPager2 pager2;
     FragmentAdapter adapter;
 
-    private FirebaseAuth mAuth;
-    private TextView text_ID, text_name, text_email;
-    private ImageView image_user;
-    private Button button_logout;
-    private Button button_delete_account;
-    private GoogleSignInClient mGoogleSignInClient;
-    private GoogleSignInOptions gso;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        image_user = findViewById(R.id.image_user);
-        text_ID = findViewById(R.id.text_ID);
-        text_name = findViewById(R.id.text_name);
-        text_email = findViewById(R.id.text_email);
-        button_logout = findViewById(R.id.btnLogout);
-        button_delete_account = findViewById(R.id.btnDeleteAccount);
-
-        mAuth = FirebaseAuth.getInstance();
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-
-        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build();
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-
-        text_ID.setText(currentUser.getUid());
-        text_name.setText(currentUser.getDisplayName());
-        text_email.setText(currentUser.getEmail());
-        Glide.with(this).load(currentUser.getPhotoUrl()).into(image_user);
 
         tabLayout = findViewById(R.id.tab_layout);
         pager2 = findViewById(R.id.view_pager2);
@@ -91,7 +45,6 @@ public class MainActivity extends AppCompatActivity {
             public void onTabReselected(TabLayout.Tab tab) {
 
             }
-
         });
 
         pager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
@@ -101,88 +54,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        button_logout.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-
-                mAuth.signOut();
-
-                mGoogleSignInClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
-
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-
-                        if (task.isSuccessful()) {
-                            Intent loginActivity = new Intent(getApplicationContext(), LoginActivity.class);
-                            startActivity(loginActivity);
-                            MainActivity.this.finish();
-
-                        } else {
-                            Toast.makeText(getApplicationContext(), "Could not log out with google", Toast.LENGTH_LONG).show();
-                        }
-                    }
-                });
-            }
-        });
-
-        button_delete_account.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-
-                final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-
-                GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(getApplicationContext());
-
-                if (signInAccount != null) {
-
-                    AuthCredential credential = GoogleAuthProvider.getCredential(signInAccount.getIdToken(), null);
-
-                    user.reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>() {
-
-                        @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-
-                            if (task.isSuccessful()) {
-
-                                user.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
-
-                                    @Override
-                                    public void onSuccess(Void aVoid) {
-
-                                        Log.d("MainActivity", "onSuccess: User Deleted");
-                                        signOut();
-
-                                    }
-                                });
-                            } else {
-                                Log.e("MainActivity", "onComplete: Failed to delete user", task.getException());
-                            }
-                        }
-                    });
-                } else {
-                    Log.d("MainActivity", "Error: reAuthenticateUser: user account is null");
-                }
-            }
-        });
     }
 
-    private void signOut() {
-
-        FirebaseAuth.getInstance().signOut();
-
-        mGoogleSignInClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
-
-            @Override
-            public void onComplete(@NonNull Task<Void> task) {
-
-                Intent IntentLoginActivity = new Intent(getApplicationContext(), LoginActivity.class);
-                IntentLoginActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(IntentLoginActivity);
-                MainActivity.this.finish();
-
-            }
-        });
-    }
 }
--- app/src/main/java/com/example/barbershop/Screen4.java
+++ app/src/main/java/com/example/barbershop/Screen4.java
@@ -1,12 +1,39 @@
 package com.example.barbershop;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.bumptech.glide.Glide;
+import com.example.barbershop.models.User;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.jetbrains.annotations.NotNull;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +50,17 @@ public class Screen4 extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private FirebaseAuth mAuth;
+    private TextView text_role, text_name, text_email;
+    private ImageView image_user;
+    private Button button_logout;
+    private Button button_delete_account;
+    private GoogleSignInClient mGoogleSignInClient;
+    private GoogleSignInOptions gso;
+
+    private FirebaseDatabase database;
+    private DatabaseReference userTable;
 
     public Screen4() {
         // Required empty public constructor
@@ -49,6 +87,8 @@ public class Screen4 extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -56,9 +96,120 @@ public class Screen4 extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_screen4, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view_screen4 = inflater.inflate(R.layout.fragment_screen4, container, false);
+
+        image_user = view_screen4.findViewById(R.id.image_user);
+        text_role = view_screen4.findViewById(R.id.text_role);
+        text_name = view_screen4.findViewById(R.id.text_name);
+        text_email = view_screen4.findViewById(R.id.text_email);
+        button_logout = view_screen4.findViewById(R.id.btnLogout);
+        button_delete_account = view_screen4.findViewById(R.id.btnDeleteAccount);
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build();
+        mGoogleSignInClient = GoogleSignIn.getClient(getActivity(), gso);
+
+        database = FirebaseDatabase.getInstance();
+        userTable = database.getReference("User");
+
+        userTable.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+
+                User user = snapshot.child(currentUser.getEmail().replaceAll("\\p{Punct}", "")).getValue(User.class);
+
+                if (user.getRole().equals("Client")) {
+                    text_role.setText("Cliente");
+                } else {
+                    text_role.setText("Administrador");
+                }
+                text_name.setText(user.getFullName());
+                text_email.setText(user.getEmail());
+                Glide.with(Screen4.this).load(currentUser.getPhotoUrl()).into(image_user);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+            }
+        });
+
+        button_logout.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                mAuth.signOut();
+
+                mGoogleSignInClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                        if (task.isSuccessful()) {
+                            Intent loginActivity = new Intent(getActivity().getApplicationContext(), LoginActivity.class);
+                            startActivity(loginActivity);
+                            getActivity().finish();
+
+                        } else {
+                            Toast.makeText(getActivity().getApplicationContext(), "Could not log out with google", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        button_delete_account.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(getActivity().getApplicationContext());
+
+                if (signInAccount != null) {
+                    AuthCredential credential = GoogleAuthProvider.getCredential(signInAccount.getIdToken(), null);
+                    user.reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>() {
+
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+
+                            if (task.isSuccessful()) {
+                                user.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Log.d("MainActivity", "onSuccess: User Deleted");
+                                        signOut();
+                                    }
+                                });
+                            } else {
+                                Log.e("MainActivity", "onComplete: Failed to delete user", task.getException());
+                            }
+                        }
+                    });
+                } else {
+                    Log.d("MainActivity", "Error: reAuthenticateUser: user account is null");
+                }
+            }
+        });
+
+        return view_screen4;
+    }
+
+    private void signOut() {
+        FirebaseAuth.getInstance().signOut();
+        mGoogleSignInClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                Intent IntentLoginActivity = new Intent(getActivity().getApplicationContext(), LoginActivity.class);
+                IntentLoginActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(IntentLoginActivity);
+                getActivity().finish();
+            }
+        });
     }
 }
