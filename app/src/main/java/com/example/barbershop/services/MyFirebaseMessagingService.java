9d59bd64a3b13fb3894d2d751ae0b35e5e656113
Elvin González <60045534+elvinlab@users.noreply.github.com>
2021-06-26 03:00:24 -0600

    Notifications with node + realtime Database server

---
+++ app/src/main/java/com/example/barbershop/services/MyFirebaseMessagingService.java
@@ -0,0 +1,104 @@
+/**
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.barbershop.services;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+
+import com.example.barbershop.R;
+import com.example.barbershop.activities.LoginActivity;
+import com.example.barbershop.activities.MainActivity;
+import com.example.barbershop.controllers.Screen2;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * NOTE: There can only be one service in each app that receives FCM messages. If multiple
+ * are declared in the Manifest then the first one will be chosen.
+ * <p>
+ * In order to make this Java sample functional, you must remove the following from the Kotlin messaging
+ * service in the AndroidManifest.xml:
+ * <p>
+ * <intent-filter>
+ * <action android:name="com.google.firebase.MESSAGING_EVENT" />
+ * </intent-filter>
+ */
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = "FirebaseMessagingService";
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        String notificationTitle = null, notificationBody = null;
+
+        // Check if message contains a notification payload
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Name: " + remoteMessage.getNotification().getTitle());
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+            notificationTitle = remoteMessage.getNotification().getTitle();
+            notificationBody = remoteMessage.getNotification().getBody();
+        }
+
+        // If you want to fire a local notification (that notification on the top of the phone screen)
+        // you should fire it from here
+       // sendLocalNotification(notificationTitle, notificationBody);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        String NOTIFICATION_CHANNEL_ID = "my_channel_id_01";
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "My Notifications", NotificationManager.IMPORTANCE_HIGH);
+
+            // Configure the notification channel.
+            notificationChannel.setDescription("Channel description");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
+            notificationChannel.enableVibration(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+
+        notificationBuilder.setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.drawable.ic_stat_ic_notification)
+                .setTicker("Elvin")
+                //     .setPriority(Notification.PRIORITY_MAX)
+                .setContentTitle(notificationTitle)
+                .setContentText(notificationBody)
+                .setContentInfo("Info");
+
+        notificationManager.notify(/*notification id*/1, notificationBuilder.build());
+    }
+
+}
