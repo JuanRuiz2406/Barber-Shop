8a40526b4c45aba9d24884f00bc0ecfa1a9e50fb
dvperga <diegoskate98@gmail.com>
2021-06-26 15:03:55 -0600

    Barra de busqueda

--- app/src/main/java/com/example/barbershop/activities/MainActivity.java
+++ app/src/main/java/com/example/barbershop/activities/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.barbershop.activities;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.FragmentManager;
 import androidx.viewpager2.widget.ViewPager2;
 
@@ -20,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setSoftInputMode(
+                WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
         setContentView(R.layout.activity_main);
 
         FirebaseMessaging. getInstance () .subscribeToTopic ("pushNotifications");
