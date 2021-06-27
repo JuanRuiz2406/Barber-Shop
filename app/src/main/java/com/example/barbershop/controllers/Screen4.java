c3012f88a02ecfa41e67202f99d305397c93bb72
Elvin González <60045534+elvinlab@users.noreply.github.com>
2021-06-26 23:56:06 -0600

    Update Screen4.java

--- app/src/main/java/com/example/barbershop/controllers/Screen4.java
+++ app/src/main/java/com/example/barbershop/controllers/Screen4.java
@@ -46,7 +46,7 @@ import org.jetbrains.annotations.NotNull;
 public class Screen4 extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
